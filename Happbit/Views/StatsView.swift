93255f528f78e3019e1121bdb62874f1dfa3c25f
Higorami <fyleas.quaile@gmail.com>
2024-03-13 11:44:26 +0100

    Première partie view star

--- Happbit/Views/StatsView.swift
+++ Happbit/Views/StatsView.swift
@@ -10,7 +10,52 @@ import SwiftUI
 struct StatsView: View {
     var body: some View {
         VStack(spacing: 16){
+            HStack(spacing:16){
+                Image(systemName: "flame.fill")
+                    .padding(.leading, 16)
+                    .font(.title)
+                    .foregroundColor(.red)
+                Text("Streak")
+                    .font(.largeTitle)
+                    
+                Spacer()
+            }
             
+            HStack(spacing: 16){
+                Spacer()
+                VStack(spacing: 16){
+                    Text("3")
+                        .font(.title2)
+                    Text("Current")
+                        .font(.title2)
+                }
+                Spacer()
+                VStack(spacing: 16){
+                    Text("15")
+                        .font(.title2)
+                    Text("Best")
+                        .font(.title2)
+                }
+                Spacer()
+            }
+            
+            HStack(spacing: 16){
+                Image(systemName: "arrow.up.right.circle")
+                    .padding(.leading, 16)
+                    .font(.title)
+                    .foregroundColor(.green)
+                Text("Stats")
+                    .font(.largeTitle)
+                Spacer()
+            }
+            .padding(.top, 40)
+            
+            VStack(spacing: 16){
+                //Picker("TimeStats", selection: )
+                
+            }
+            
+            Spacer()
         }
     }
 }
