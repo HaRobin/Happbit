dae95eea85be9af3bd6b904916f8558d745f61c3
Higorami <fyleas.quaile@gmail.com>
2024-03-13 11:09:42 +0100

    add view

--- Happbit/Views/AddHabitView.swift
+++ Happbit/Views/AddHabitView.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct AddHabitView: View {
+    
+    @State var habitTitle: String = ""
+    
+    @EnvironmentObject var habitViewModel: HabitViewModel
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16){
+            
+            TextField("Enter a habit", text: $habitTitle)
+                .padding(.horizontal)
+                .frame(height: 55)
+                .background(Color(.systemGray5))
+                .cornerRadius(10)
+            
+            Button{
+                habitViewModel.addHabit(title: habitTitle)
+                presentationMode.wrappedValue.dismiss()
+            } label: {
+                Text("Save")
+                    .font(.headline)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            
+            Spacer()
+        }
+        .padding(16)
+        .navigationTitle("Add a Habit")
     }
 }
 
 struct AddHabitView_Previews: PreviewProvider {
     static var previews: some View {
-        AddHabitView()
+        NavigationView{
+            AddHabitView()
+        }
     }
 }
