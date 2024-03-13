c7d845af737258a0f600c18cfaa8c32926a98f88
Higorami <fyleas.quaile@gmail.com>
2024-03-13 11:13:50 +0100

    create stat view

---
+++ Happbit/Views/StatsView.swift
@@ -0,0 +1,22 @@
+//
+//  StatsView.swift
+//  Happbit
+//
+//  Created by quaile fyleas on 13/03/2024.
+//
+
+import SwiftUI
+
+struct StatsView: View {
+    var body: some View {
+        VStack(spacing: 16){
+            
+        }
+    }
+}
+
+struct StatsView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatsView()
+    }
+}
