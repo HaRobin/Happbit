aa3fb09985acc2b43371af81ea736e418a6ddc95
HaRobin <robin.halm68@gmail.com>
2024-03-20 11:45:32 +0100

    Local notification WiP

--- Happbit/Views/SettingsView.swift
+++ Happbit/Views/SettingsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SettingsView: View {
     
+    @State var isPermissionGranted: Bool = false
     @State var nbNutification: Int = 1
     
     var body: some View {
@@ -16,25 +17,72 @@ struct SettingsView: View {
         NavigationView {
             VStack {
                 
-                HStack {
-                    Text("Number of notification :")
-                    
-                    Spacer()
-                    
-                    TextField("Enter expense amount", value: $nbNutification, formatter: NumberFormatter())
-                        .keyboardType(.numberPad)
-                        .padding(.horizontal)
-                        .frame(width: 80, height: 55)
-                        .background(Color(.systemGray5))
-                        .cornerRadius(10)
+                if !isPermissionGranted {
+                    Button("Request Permission") {
+                        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+                            DispatchQueue.main.async {
+                                self.isPermissionGranted = granted
+                            }
+                            if granted {
+                                print("All set!")
+                            } else if let error = error {
+                                print(error.localizedDescription)
+                            }
+                        }
+                    }
+                            
                 }
+                else {
+                    HStack {
+                        Text("Number of notification :")
+                            
+                        
+                        Spacer()
+                        
+                        TextField("Enter expense amount", value: $nbNutification, formatter: NumberFormatter())
+                            .keyboardType(.numberPad)
+                            .padding(.horizontal)
+                            .frame(width: 40, height: 30)
+                            .background(Color(.systemGray5))
+                            .cornerRadius(10)
+                        
+                        Spacer()
+                    }
+                }
+
                 
                 Spacer()
+             
+//                Button("test notification") {
+//                    let content = UNMutableNotificationContent()
+//                    content.title = "Happbit"
+//                    content.subtitle = "Do your task"
+//                    content.sound = UNNotificationSound.default
+//
+//                    // show this notification five seconds from now
+//                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+//
+//                    // choose a random identifier
+//                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+//
+//                    // add our notification request
+//                    UNUserNotificationCenter.current().add(request)
+//                }
+                
+                Spacer()
+                
                 
             }
             
             .padding()
             .navigationTitle("Settings")
+        }
+        .onAppear {
+            UNUserNotificationCenter.current().getNotificationSettings { settings in
+                DispatchQueue.main.async {
+                    self.isPermissionGranted = settings.authorizationStatus == .authorized
+                }
+            }
         }
     }
 }
@@ -46,3 +94,26 @@ struct SettingsView_Previews: PreviewProvider {
 
     }
 }
+
+//let content = UNMutableNotificationContent()
+//content.title = "Feed the cat"
+//content.subtitle = "It looks hungry"
+//content.sound = UNNotificationSound.default
+//
+//// Set up the date components for the time you want the notification to trigger
+//var dateComponents = DateComponents()
+//dateComponents.hour = 8  // 8 AM
+//dateComponents.minute = 0
+//
+//// Create the trigger for the notification
+//let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+//
+//// Create the notification request with a unique identifier, your content, and the trigger
+//let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+//
+//// Schedule the notification request
+//UNUserNotificationCenter.current().add(request) { error in
+//    if let error = error {
+//        print("Error scheduling notification: \(error)")
+//    }
+//}
